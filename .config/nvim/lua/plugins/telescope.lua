77483e48627a2b0c587927ab5135a40f85b5337b
Christopher Bown <cbown75@gmail.com>
2024-04-03 19:33:38 -0700

    chore(.config/nvim/lua/plugins): adding new plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -55,4 +55,12 @@ return {
 		keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
+}, {
+	"nvim-telescope/telescope-fzf-native.nvim",
+	build = vim.fn.executable("make") == 1 and "make"
+		or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	enabled = vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1,
+	config = function()
+		require("telescope").load_extension("fzf")
+	end,
 }
