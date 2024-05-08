5deeef3f06e5f84e9a1c323bede3760941e83ed1
Christopher Bown <cbown75@gmail.com>
2024-05-08 12:32:34 -0700

    chore: updating spacing in alpha

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -30,15 +30,15 @@ return {
 		local builtin = require("telescope.builtin")
 
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ne", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left<CR>"),
+			dashboard.button("e", " > New File", "<cmd>ene<CR>"),
+			dashboard.button("SPC ne", " > Toggle file explorer", "<cmd>Neotree filesystem reveal left<CR>"),
 			dashboard.button(
 				"SPC ff",
 				"󰱼 > Find File",
 				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
 			),
-			dashboard.button("SPC fg", "  > Live Grep", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("SPC fg", " > Live Grep", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC wr", "󰁯 > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
