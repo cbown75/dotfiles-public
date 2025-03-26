6bfd208f7ab35b88acf7ef8a94e9b90a04651ff4
Christopher Bown <cbown75@gmail.com>
2025-03-26 15:08:15 -0700

    chore(.config/nvim/lua/plugins): working on dap and lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,76 +1,79 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"golangci_lint_ls",
-					"bashls",
-					"rust_analyzer",
-					"dockerls",
-					"docker_compose_language_service",
-					"gopls",
-					"helm_ls",
-					--"html",
-					"jsonls",
-					"jqls",
-					"lua_ls",
-					"marksman",
-					--"nil_ls",
-					"pylsp",
-					"ruff",
-					"tflint",
-					"terraformls",
-					"yamlls",
-				},
-				-- auto-install configured servers (with lspconfig)
-				automatic_installation = true, -- not the same as ensure_installed
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "golangci_lint_ls",
+          "bashls",
+          "rust_analyzer",
+          "dockerls",
+          "docker_compose_language_service",
+          "gopls",
+          "helm_ls",
+          --"html",
+          "jsonls",
+          "jqls",
+          "lua_ls",
+          "marksman",
+          --"nil_ls",
+          "pylsp",
+          "ruff",
+          "tflint",
+          "terraformls",
+          "yamlls",
+        },
+        -- auto-install configured servers (with lspconfig)
+        automatic_installation = true, -- not the same as ensure_installed
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+    },
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,52 +1,54 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-		"jayp0521/mason-null-ls.nvim",
-	},
-	config = function()
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				"ruff",
-				"prettier",
-				"shfmt",
-				"yamlfmt",
-				"isort",
-				"black",
-			},
-			automatic_installation = true,
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim",
+  },
+  config = function()
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "ruff",
+        "prettier",
+        "shfmt",
+        "yamlfmt",
+        "isort",
+        "black",
+      },
+      automatic_installation = true,
+    })
 
-		local null_ls = require("null-ls")
-		local sources = {
-			require("none-ls.diagnostics.eslint_d"),
-			require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
-			require("none-ls.formatting.ruff_format"),
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "markdown", "yaml", "css", "html" } }),
-			--null_ls.builtins.diagnostics.erb_lint,
-			--null_ls.builtins.diagnostics.rubocop,
-			--null_ls.builtins.formatting.rubocop,
-			null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
-		}
+    local null_ls = require("null-ls")
+    local sources = {
+      require("none-ls.diagnostics.eslint_d"),
+      require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+      require("none-ls.formatting.ruff_format"),
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "markdown", "css", "html" } }),
+      --null_ls.builtins.diagnostics.erb_lint,
+      null_ls.builtins.diagnostics.yamllint,
+      --null_ls.builtins.formatting.rubocop,
+      null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
+      null_ls.builtins.formatting.terraform_fmt,
+      null_ls.builtins.formatting.yamlfmt,
+    }
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		require("null-ls").setup({
-			sources = sources,
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end
-			end,
-		})
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    require("null-ls").setup({
+      sources = sources,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
+      end,
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
